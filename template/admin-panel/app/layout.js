3cfe3f0758eaa46e198691940ffacd468d5860d1
coded-circuit <kabya25ranjan@gmail.com>
2025-10-17 23:34:26 +0530

    Feat: layout.js added

--- template/admin-panel/app/layout.js
+++ template/admin-panel/app/layout.js
@@ -1,1 +1,19 @@
-import "../styles/globals.css"
+import '../styles/globals.css';
+import { Inter } from 'next/font/google';
+const inter = Inter({ subsets: ['latin'] });
+
+
+export const metadata = {
+  title: 'Admin Panel',
+  description: 'Custom Admin Dashboard for SWC Projects  ',
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        {children}
+      </body>
+    </html>
+  );
+}
